a1f146c31d11dd7902b0e64c5982ccfa81b4eb59
amakenzi <almaz.t97@gmail.com>
2024-07-18 11:39:08 +0500

    prod api config

--- lib/api/getTypeData.ts
+++ lib/api/getTypeData.ts
@@ -1,5 +1,7 @@
+import { BASE_API } from "../constants/const";
+
 export const getTypeData = async (type: string) => {
-  const res = await fetch("http://localhost:3000/api/components/type/" + type);
+  const res = await fetch(`${BASE_API}/api/components/type/` + type);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
